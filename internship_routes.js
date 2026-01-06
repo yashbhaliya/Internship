bc634ed8b052035c53e30bc84e87e84f20ff691f
yash <bhaliyayash595@gmail.com>
2026-01-06 17:04:47 +0530

    first commit

--- internship_routes.js
+++ internship_routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Internship = require("./internship");
+const mongoose = require("mongoose");
 
 /* CREATE Internship */
 router.post("/", async (req, res) => {
